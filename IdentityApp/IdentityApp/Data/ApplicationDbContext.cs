de129502104100ee91e94f2b052616af62ff0745
itiscs <moudroff@gmail.com>
2026-05-08 17:02:43 +0300

    Identity App

---
+++ IdentityApp/IdentityApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ IdentityApp/IdentityApp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+    }
+}
